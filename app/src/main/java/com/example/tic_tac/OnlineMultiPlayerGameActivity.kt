86342b125d0170147dba7e7e4768c19f982ca90a
sadulla <ubaydullayevsadulla843@gmail.com>
2022-07-26 11:32:31 +0500

    slightly changed the appearance of the ui

--- app/src/main/java/com/example/tic_tac/OnlineMultiPlayerGameActivity.kt
+++ app/src/main/java/com/example/tic_tac/OnlineMultiPlayerGameActivity.kt
@@ -1,10 +1,12 @@
 package com.example.tic_tac
 
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
+import android.view.Window
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
